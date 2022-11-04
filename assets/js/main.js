613b0f6c3b3231edcffb004ebef8892fb950f00f
Paulo Marques <paulomarques.rs1@gmail.com>
2022-11-04 05:13:59 -0300

    Add persistencia de itens

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,79 +1,40 @@
-const form = document.getElementById("novoItem")
-const lista = document.getElementById("lista")
-const itens = JSON.parse(localStorage.getItem("itens")) || []
+const form = document.getElementById('novoItem');
+const lista = document.getElementById('lista');
+const items = JSON.parse(localStorage.getItem("items")) || []
 
-itens.forEach( (elemento) => {
-    criaElemento(elemento)
-} )
+items.forEach(element => {
+    console.log(element)
+    createElement(element);
+});
 
-form.addEventListener("submit", (evento) => {
-    evento.preventDefault()
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-    const nome = evento.target.elements['nome']
-    const quantidade = evento.target.elements['quantidade']
-
-    const existe = itens.find( elemento => elemento.nome === nome.value )
-
-    const itemAtual = {
-        "nome": nome.value,
-        "quantidade": quantidade.value
-    }
-
-    if (existe) {
-        itemAtual.id = existe.id
-        
-        atualizaElemento(itemAtual)
-
-        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
-    } else {
-        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
-
-        criaElemento(itemAtual)
-
-        itens.push(itemAtual)
-    }
-
-    localStorage.setItem("itens", JSON.stringify(itens))
-
-    nome.value = ""
-    quantidade.value = ""
-})
-
-function criaElemento(item) {
-    const novoItem = document.createElement("li")
-    novoItem.classList.add("item")
-
-    const numeroItem = document.createElement("strong")
-    numeroItem.innerHTML = item.quantidade
-    numeroItem.dataset.id = item.id
-    novoItem.appendChild(numeroItem)
+    const nome = event.target.elements['nome'].value;
+    const quantidade = event.target.elements['quantidade'].value;
     
-    novoItem.innerHTML += item.nome
+    const currentItem = {
+        "nome": nome,
+        "quantidade": quantidade 
+    };
 
-    novoItem.appendChild(botaoDeleta(item.id))
+    createElement(currentItem);
+    
+    items.push(currentItem);
+    localStorage.setItem("items", JSON.stringify(items));
+    
+    form.reset();
+});
 
-    lista.appendChild(novoItem)
-}
+function createElement(item){
+    const newItem = document.createElement('li');
+    newItem.classList.add('item');
 
-function atualizaElemento(item) {
-    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
-}
+    const numberItem = document.createElement('strong');
+    numberItem.innerHTML = item.quantidade;
 
-function botaoDeleta(id) {
-    const elementoBotao = document.createElement("button")
-    elementoBotao.innerText = "X"
+    newItem.appendChild(numberItem);
+    newItem.innerHTML += item.nome;
 
-    elementoBotao.addEventListener("click", function() {
-        deletaElemento(this.parentNode, id)
-    })
-
-    return elementoBotao
-}
-
-function deletaElemento(tag, id) {
-    tag.remove()
-
-    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
-
-    localStorage.setItem("itens", JSON.stringify(itens))
-}
+    lista.appendChild(newItem);
+};
